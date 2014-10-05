aa8735b6cd44f964365a5403bf9d5751c72d2bf4
Martin Tomasek <martin@toms-cz.com>
2014-10-05 16:40:26 +0200

    Showcase project added

---
+++ examples/Showcase/src/main/java/com/tomscz/af/showcase/forms/PersonFormView.java
@@ -0,0 +1,13 @@
+package com.tomscz.af.showcase.forms;
+
+import javax.swing.JFrame;
+
+public class PersonFormView extends JFrame{
+    
+    private static final long serialVersionUID = 1L;
+
+    public PersonFormView(){
+        this.setVisible(true);
+    }
+    
+}
---
+++ examples/Showcase/src/main/java/com/tomscz/af/showcase/main/Main.java
@@ -0,0 +1,12 @@
+package com.tomscz.af.showcase.main;
+
+import com.tomscz.af.showcase.forms.PersonFormView;
+
+public class Main {
+
+    public static void main(String[] args) {
+       PersonFormView personView = new PersonFormView();
+
+    }
+
+}
